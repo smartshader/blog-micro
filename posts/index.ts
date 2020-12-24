6057fe6afe8de8bb0cac82792583398f065967de
Imad Jerra <imad.jerra@ariv.ca>
2020-12-24 17:12:55 -0500

    Add posts get and post methods

---
+++ posts/index.ts
@@ -0,0 +1,34 @@
+import express from 'express';
+import { randomBytes } from "crypto";
+import { StatusCodes } from "http-status-codes";
+import bodyParser from 'body-parser';
+
+const PORT = 4000;
+
+const app = express();
+app.use(bodyParser.json());
+
+interface Post {
+    id: string;
+    title: string;
+}
+
+const posts: {[id: string]: Post} = {};
+
+app.get('/posts', (req, res) => {
+    res.send(posts);
+});
+
+app.post('/posts', (req, res) => {
+    const id = randomBytes(4).toString('hex');
+    const { title } = req.body;
+
+    posts[id] = {id, title};
+
+    res.status(StatusCodes.ACCEPTED);
+    res.send(posts[id]);
+});
+
+app.listen(PORT, () => {
+    console.log(`⚡️[server]: Server is running at http://localhost:${PORT}`);
+});
