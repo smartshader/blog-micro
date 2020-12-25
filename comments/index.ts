e0af536bd0f1bfcfaf63f17d72bf9ac9b5cdf57f
Imad Jerra <imad.jerra@ariv.ca>
2020-12-25 08:23:19 -0500

    Add comments get and post endpoints

--- comments/index.ts
+++ comments/index.ts
@@ -1,9 +1,34 @@
 import express from 'express';
+import bodyParser from "body-parser";
+import { randomBytes } from "crypto";
+
+const PORT = 4001;
 
 const app = express();
-const PORT = 8001;
+app.use(bodyParser.json())
 
-app.get('/', (req, res) => res.send('Express + TypeScript Server'));
+interface comment {
+    id: string;
+    content: string;
+}
+
+const commentsByPostId: {[id: string]: comment[]} = {};
+
+app.get('/posts/:id/comments', (req, res) => {
+    res.send(commentsByPostId[req.params.id] || []);
+});
+
+app.post('/posts/:id/comments', (req, res) => {
+    const commentId = randomBytes(4).toString('hex');
+    const { content } = req.body;
+
+    const comments: comment[] = commentsByPostId[req.params.id] || [];
+    comments.push({ id: commentId, content })
+    commentsByPostId[req.params.id] = comments;
+
+    res.status(201);
+    res.send(comments);
+});
 
 app.listen(PORT, () => {
     console.log(`⚡️[server]: Server is running at https://localhost:${PORT}`);
